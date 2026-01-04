e88d67317b6b5368b3c1b9e91e0bccd96424f9c1
edilenealima <edilenealima80@gmail.com>
2026-01-04 14:41:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-function mostrarTela(id) {
-  const telas = document.querySelectorAll('.tela');
-  telas.forEach(tela => {
-    tela.style.display = 'none';
-    tela.classList.remove('ativa');
+<script>
+function mostrar(id) {
+  document.querySelectorAll('.section').forEach(sec => {
+    sec.classList.remove('active');
   });
-
-  const telaAtiva = document.getElementById(id);
-  if (telaAtiva) {
-    telaAtiva.style.display = 'block';
-    telaAtiva.classList.add('ativa');
-  }
+  document.getElementById(id).classList.add('active');
 }
-
-// garante que a home aparece ao abrir
-document.addEventListener("DOMContentLoaded", () => {
-  mostrarTela('inicio');
-});
+</script>
